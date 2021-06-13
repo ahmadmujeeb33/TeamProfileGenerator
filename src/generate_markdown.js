328e766badea710bae7c1a3cca7793d601c71286
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-06-13 19:51:45 -0400

    initlized template

---
+++ src/generate_markdown.js
@@ -0,0 +1,31 @@
+
+
+function generateHTML(response){
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <p>my name is ${response.name}</p>
+        <p>my name is ${response.location}</p>
+
+        <ul>
+            ${generateArrayElements(response.myArray)}
+        </ul>
+      
+        
+    </body>
+    </html>` 
+}
+
+  // <ul>
+        //     ${generateArrayElements(response.myArray)}
+        // </ul>
+
+module.exports = {
+    generateHTML,
+}
