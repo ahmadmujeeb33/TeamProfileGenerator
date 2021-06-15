20e0126055d66531732b0140a147d639f534e4e4
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-06-15 00:00:19 -0400

    updated

--- src/generate_markdown.js
+++ src/generate_markdown.js
@@ -1,4 +1,5 @@
 function generateArrayElements (array) {
+    console.log(array);
     let returnString  = '';
     array.forEach(element => {
         returnString += `<p> ${element["name"]} </p>`
@@ -28,13 +29,26 @@ function generateHTML(data){
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
         <title>Document</title>
+        <style>
+
+        </style>
     </head>
     <body>
        
 
         <div>
             ${generateArrayElements(data)}
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+                </div>
         </div>
       
         
